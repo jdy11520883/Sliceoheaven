7d24094bdceb77a3979f31b20171f5e83ae573f6
jdy11520883 <3862483694@qq.com>
2025-03-06 00:13:28 +0800

    Update SliceoHeaven.java

--- SliceoHeaven.java
+++ SliceoHeaven.java
@@ -1,8 +1,6 @@
 public class SliceoHeaven {
     public String storeAddress;
-
-    public  String storeMenu;
-
+    public String storeMenu;
     public String storeName;
     public long storePhone;
     public String storeEmail;
@@ -14,67 +12,68 @@ public class SliceoHeaven {
     public static final String DEF_ORDER_ID = "DEF-SOH-099";
     public static final String DEF_PIZZA_INGREDIENTS = "Mozzarella Cheese";
     public static final double DEF_ORDER_TOTAL = 15.00;
+
+    private String orderID;
+    private double orderTotal;
+
     public SliceoHeaven() {
         this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
         this.orderTotal = DEF_ORDER_TOTAL;
         this.sides = ""; 
         this.drinks = ""; 
     }
-    public SliceoHeaven(int orderID, String pizzaIngredients, double orderTotal) {
+
+    public SliceoHeaven(String orderID, String pizzaIngredients, double orderTotal) {
         this.orderID = orderID;
         this.pizzaIngredients = pizzaIngredients;
         this.orderTotal = orderTotal;
     }
 
-    private String orderID;
-    private double orderTotal;
-
-    public void takeOrder(String id, String details, double total){
+    public void takeOrder(String id, String details, double total) {
         orderID = id;
         orderTotal = total;
 
         System.out.println("Order accepted!");
-
         System.out.println("Order is being prepared");
 
-        try{
+        try {
             Thread.sleep(5000);
-        } catch (InterruptedException e){
-            System.out.println("Order is ready for pickup!");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Order preparation was interrupted!");
         }
 
         System.out.println("Your order is ready!");
-
         printReceipt();
-
-
     }
-    
-    private void printReceipt(){
-        System.out.println("********RECEIPT********");
 
+    private void printReceipt() {
+        System.out.println("********RECEIPT********");
         System.out.println("Order ID: " + orderID);
         System.out.println("Order Total: " + orderTotal);
     }
-    
-}
-public int getOrderID() {
-    return orderID;
-}
-public void setOrderID(int orderID) {
-    this.orderID = orderID;
-}
-public String getPizzaIngredients() {
-    return pizzaIngredients;
-}
-public void setPizzaIngredients(String pizzaIngredients) {
-    this.pizzaIngredients = pizzaIngredients;
-}
-public double getOrderTotal() {
-    return orderTotal;
-}
-public void setOrderTotal(double orderTotal) {
-    this.orderTotal = orderTotal;
-}
-private void printReceipt() {
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getPizzaIngredients() {
+        return pizzaIngredients;
+    }
+
+    public void setPizzaIngredients(String pizzaIngredients) {
+        this.pizzaIngredients = pizzaIngredients;
+    }
+
+    public double getOrderTotal() {
+        return orderTotal;
+    }
+
+    public void setOrderTotal(double orderTotal) {
+        this.orderTotal = orderTotal;
+    }
 }
